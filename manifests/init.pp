9f74a73db7d81ee9e839cece71a0b01c5ac71ee9
Dennis Reimann <mail@dennisreimann.de>
2013-04-08 23:00:52 +0200

    Make the linter happy

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,7 @@ class spotify {
 
   package { 'Spotify':
     provider => 'compressed_app',
-    source   => "http://download.spotify.com/SpotifyInstaller.zip",
+    source   => 'http://download.spotify.com/SpotifyInstaller.zip',
   }
   ~> exec { 'Install Spotify':
     command     => "/usr/bin/open ${install_file}",
